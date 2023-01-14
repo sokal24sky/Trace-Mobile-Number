18193bee20e6dd8ed3b09bbd6311004cda6a394e
Hiran Sarfaraz <75323129+sokal24sky@users.noreply.github.com>
2023-01-14 16:41:27 +0600

    Add files via upload

---
+++ number.py
@@ -0,0 +1,12 @@
+import phonenumbers
+from phonenumbers import timezone, geocoder, carrier
+
+number=input('Enter Your Number With County Code: ')
+phone=phonenumbers.parse(number)
+time=timezone.time_zones_for_number(phone)
+carr=carrier.name_for_number(phone, 'en')
+reg=geocoder.description_for_number(phone, 'en')
+print(phone)
+print(time)
+print(carr)
+print(reg)
